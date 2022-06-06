98987588c59d3623898f5536ac0107c0c6b562ee
Fernando Valenzuela <fvp314@gmail.com>
2022-06-06 17:42:22 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const amarilloAprilDailyWindSpeedsArray = [19.1, 17.9, 21.4, 7.6, 17.3, 19.2, 12.5, 12.8, 15.0, 18.7, 23.0, 11.8, 5.8, 14.9, 21.2, 16.6, 15.5, 10.3, 8.2, 12.9, 14.5, 8.5, 16.7, 7.3, 18.5, 12.8, 16.3, 13.8, 17.7]
+
+//https://www.wunderground.com/history/monthly/us/tx/amarillo/KAMA/date/2020-4
+const totalWindSpeeds = amarilloAprilDailyWindSpeedsArray.reduce(function(total, currentElement){
+  return (total + currentElement)
+})
+console.log(`The wind blew at an average of ${totalWindSpeeds/amarilloAprilDailyWindSpeedsArray.length}`)
